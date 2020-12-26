84c78436ec1c44467a1828eacd6852691ab34ab2
nawa aripurna <nap.aripurna@gmail.com>
2020-12-26 21:48:09 +0800

    dynamic xml

--- main.rb
+++ main.rb
@@ -1,21 +1,23 @@
 require 'nokogiri'
 require 'csv'
 
-for i in 1..17 do
+Dir.glob(File.expand_path("carts/*.xml", __dir__)).each do |file|
     
-    file = File.read(File.expand_path("carts/chart#{i}.xml", __dir__))
-    xml = Nokogiri::XML(file)
+    file_string = File.read(file)
+    xml = Nokogiri::XML(file_string)
+    
+    file_name = "x100_#{file.split('/').last.gsub('.xml', '.csv')}"
 
-    File.write(File.expand_path("outputs/chart#{i}.csv", __dir__), "")
-    puts "outputs/chart#{i}.csv"
-    CSV.open(File.expand_path("outputs/chart#{i}.csv", __dir__), "wb") do |csv|
+    File.write(File.expand_path("outputs/#{file_name}", __dir__), "")
+    puts "outputs/#{file_name}"
+    CSV.open(File.expand_path("outputs/#{file_name}", __dir__), "wb") do |csv|
         headers = xml.at("//c:plotArea //c:cat").xpath(". //c:v").map{ |i| i.text.to_s.to_i }
         headers.insert(0, "Category")
         csv << headers
 
         xml.xpath("//c:ser").each do |item|
             title = item.xpath(". //c:tx //c:strRef //c:v").text.to_s
-            values = item.xpath(". //c:val //c:numRef //c:v").map { |i| i.text.to_s.to_f }
+            values = item.xpath(". //c:val //c:numRef //c:v").map { |i| i.text.to_s.to_f * 100 }
             row = values.insert(0, title)
             puts row
             puts "---------------------------------------------"
